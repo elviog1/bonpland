f845374f32ba94bccb3833c62203fe49273efabb
Elvio Galeano <elvio.galeano827@gmail.com>
2022-11-02 00:52:46 -0300

    se agrego el componente login

---
+++ src/components/Login.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import '../styles/Login.css'
+import swAlert from 'sweetalert';
+import { useNavigate } from 'react-router-dom';
+export default function Login() {
+    const navigate = useNavigate()
+    const login = e =>{
+        e.preventDefault()
+        const email = e.target.email.value
+        const password = e.target.password.value
+        if(email == "banpland@gmail.com" && password == "test"){
+            swAlert("Bienvenido", "Conexion exitosa", "success")
+            navigate("/welcome")
+        }else{
+            swAlert("Error", "Usuario o contraseña incorrecta", "error")
+        }
+    }
+  return (
+    <div className='d-flex justify-content-center mt-5'>
+        <form className='form-login p-5' onSubmit={login}>
+            <h2>Iniciar sesion</h2>
+            <div className='d-flex flex-column'>
+                <label className='d-flex justify-content-start'>Correo</label>
+                <input className='login-input' type="email" name='email'/>
+            </div>
+            <div className='d-flex flex-column'>
+                <label className='d-flex justify-content-start'>Contraseña</label>
+                <input className='login-input' type="password" name='password' />
+            </div>
+            <div className='mt-3'>
+                <button className='btn-iniciar'>Iniciar</button>
+            </div>
+            <div className='login-o'>
+                <div className='login-border'></div>
+                <span className='login-span'>Ó</span>
+                <div className='login-border'></div>
+            </div>
+
+        </form>
+    </div>
+  )
+}
